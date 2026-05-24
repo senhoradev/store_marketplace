844ebee694d2fcb339665661ee8db85718ce0c8c
Joatan Feitosa <joatancarlos@gmail.com>
2026-05-23 22:38:09 -0300

    fix: max width details page

--- src/app/pages/detailsPage.tsx
+++ src/app/pages/detailsPage.tsx
@@ -92,7 +92,7 @@ export default function DetailsPage() {
   return (
     <div className="bg-background">
       <Header user={user} />
-      <main className="container mx-auto px-4 py-8">
+      <main className="container max-w-7xl mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 flex flex-col gap-6">
             <ImageGallery
